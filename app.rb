e3be37f7a31c7e370f8da505dc35676595e80457
Sterling Radcliff <radcliff@riseup.net>
2014-06-18 02:07:13 -0700

    added twilio-ruby gem; root responds with TwiML (xml)

--- app.rb
+++ app.rb
@@ -1,10 +1,14 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'twilio-ruby'
 
 set :server, 'thin'
 set :port, 2424
 
 get '/' do
-  "ride trains"
+  twiml = Twilio::TwiML::Response.new do |response|
+    response.Message "ride trains!"
+  end
+  twiml.text
 end
