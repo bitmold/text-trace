14a6d745cd1f299830a479122a8b73c338330cc5
Sterling Radcliff <radcliff@riseup.net>
2014-06-19 00:00:01 -0700

    handles twilio request to /trace and responds with TwiML

--- app.rb
+++ app.rb
@@ -10,8 +10,20 @@ set :port, 2424
 get '/' do
   erb :index
 end
+
+get '/trace' do
+  query = params[:Body]
+  bnsf_trace(query)
   twiml = Twilio::TwiML::Response.new do |response|
-    response.Message "ride trains!"
+    response.Message "container id: #{query}\ndestination: #{@destination}\neta: #{@eta}"
   end
   twiml.text
+end
+
+def bnsf_trace(query)
+  response = Typhoeus.post('http://m.bnsf.com/bnsf.was6/dillApp/rprt/QRYM?selectedValues=&spoolId=&cmd=&patName=&patAddress=&patAddress2=&patCity=&patState=&patAttn=&patZip=&patPhone=&selectStation=&hEqpInit=&hEqpNumb=&selTotal=&equipment=' + query)
+  doc = Nokogiri::HTML(response.body)
+   
+  @destination = doc.css("span#DestHub").inner_html
+  @eta = doc.css("span#ETA").inner_html
 end
